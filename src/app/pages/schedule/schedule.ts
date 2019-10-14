5389b4a48a2840b7506d25e41d8ec129d2d7b706
榊原昌彦 <sakakibara@rdlabo.jp>
2019-10-14 13:19:18 +0900

    WIP: feat(schedule): スケジュールの差し替え

--- src/app/pages/schedule/schedule.ts
+++ src/app/pages/schedule/schedule.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController, IonList, LoadingController, ModalController, ToastController, Config } from '@ionic/angular';
+import { AlertController, IonList, LoadingController, Config } from '@ionic/angular';
 
 import { ConferenceData } from '../../providers/conference-data';
 import { UserData } from '../../providers/user-data';
@@ -21,15 +21,12 @@ export class SchedulePage implements OnInit {
   excludeTracks: any = [];
   shownSessions: any = [];
   groups: any = [];
-  confDate: string;
 
   constructor(
     public alertCtrl: AlertController,
     public confData: ConferenceData,
     public loadingCtrl: LoadingController,
-    public modalCtrl: ModalController,
     public router: Router,
-    public toastCtrl: ToastController,
     public user: UserData,
     public config: Config
   ) { }
