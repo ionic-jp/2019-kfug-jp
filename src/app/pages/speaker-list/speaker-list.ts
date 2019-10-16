86ef72ff05302ba6c68489636615e8499d4491dc
榊原昌彦 <sakakibara@rdlabo.jp>
2019-10-16 20:30:11 +0900

    chore

--- src/app/pages/speaker-list/speaker-list.ts
+++ src/app/pages/speaker-list/speaker-list.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActionSheetController } from '@ionic/angular';
 
