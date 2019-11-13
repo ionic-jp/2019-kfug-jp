1918c59ce44a5460699aca25324086534252b13e
榊原昌彦 <sakakibara@rdlabo.jp>
2019-11-13 18:59:06 +0900

    ローディングスピナーをDOMで生成

--- src/app/pages/tabs-page/tabs-page.ts
+++ src/app/pages/tabs-page/tabs-page.ts
@@ -3,4 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: 'tabs-page.html',
 })
-export class TabsPage {}
+export class TabsPage {
+  ionViewDidEnter() {
+    const spinner = document.getElementById('install-loading-spinner');
+    spinner.parentNode.removeChild(spinner);
+  }
+}
